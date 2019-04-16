f147ac2eae2103da67847ce0bb33423db1f401fd
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-04-16 11:43:37 -0700

    Added actions, builded switch in charReducer, called action creator inside of componentDidMount to trigger API request. Cleaned up the code

--- src/index.js
+++ src/index.js
@@ -7,16 +7,11 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from "./reducers";
-// needed dependancies
-// applyMiddleware from redux +
-// thunk from redux-thunk +
-// logger from redux-logger +
-// rootReducer from ./reducers +
+
 
 const store = createStore(
   rootReducer,
   applyMiddleware(thunk, logger)
-  /* applyMiddleware goes here */
 );
 
 ReactDOM.render(
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,34 +2,31 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import { getCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
-    // call our action
+    this.props.getCharacters();
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
-    }
     return (
       <div className="CharactersList_wrapper">
+        { this.props.fetching && (<p>Loading...</p>) }
         <CharacterList characters={this.props.characters} />
+        {this.props.error && <p className="error">{this.props.error}</p>}
       </div>
     );
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  error: state.charsReducer.error,
+  fetching: state.charsReducer.fetching
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { getCharacters}
 )(CharacterListView);
